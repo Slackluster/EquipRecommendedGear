d5b2095e36984d081c2b1a23aaa872413fdaae60
Sluimerstand <Ruben.Sluimer@gmail.com>
2024-11-18 20:13:00 +0100

    Slight consistency tweak

--- Core.lua
+++ Core.lua
@@ -808,15 +808,15 @@ function app.Settings()
 
 	layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(C_AddOns.GetAddOnMetadata(appName, "Version")))
 
-	local cbVariable, cbName, cbTooltip = "runAfterQuest", "Run on quest completion", "Run "..app.NameShort.." when completing a quest, thus equipping any new quest rewards that are an item level upgrade."
+	local cbVariable, cbName, cbTooltip = "runAfterQuest", "Run on Quest Completion", "Run "..app.NameShort.." when completing a quest, thus equipping any new quest rewards that are an item level upgrade."
 	local cbSetting = Settings.RegisterAddOnSetting(category, appName.."_"..cbVariable, cbVariable, EquipRecommendedGear_Settings, Settings.VarType.Boolean, cbName, true)
 
-	local ddVariable, ddName, ddTooltip = "chatMessage", "Send chat message", "These settings only affect the chat message sent after quest completion."
+	local ddVariable, ddName, ddTooltip = "chatMessage", "Send Chat Message", "These settings only affect the chat message sent after quest completion."
 	local function GetOptions()
 		local container = Settings.CreateControlTextContainer()
-		container:Add(0, "Never send message", "Don't send a message in chat, even if "..app.NameShort.." has equipped an item level upgrade.")
-		container:Add(1, "Only with upgrade", "Only send a message in chat if "..app.NameShort.." has equipped an item level upgrade.")
-		container:Add(2, "Always send message", "Always send a chat message, even if "..app.NameShort.." hasn't equipped an item level upgrade.")
+		container:Add(0, "Never Send Message", "Don't send a message in chat, even if "..app.NameShort.." has equipped an item level upgrade.")
+		container:Add(1, "Only With Upgrade", "Only send a message in chat if "..app.NameShort.." has equipped an item level upgrade.")
+		container:Add(2, "Always Send Message", "Always send a chat message, even if "..app.NameShort.." hasn't equipped an item level upgrade.")
 		return container:GetData()
 	end
 	local ddSetting = Settings.RegisterAddOnSetting(category, appName.."_"..ddVariable, ddVariable, EquipRecommendedGear_Settings, Settings.VarType.Number, ddName, 1)
