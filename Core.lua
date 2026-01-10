420426ea3740297eb3791144b62420d268580bbe
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-01-10 16:03:29 +0100

    Reference API and L consistently

--- Core.lua
+++ Core.lua
@@ -7,6 +7,7 @@ local appName, app = ...	-- Returns the addon name and a unique table
 app.locales = {}	-- Localisation table
 app.api = {}	-- Our "API" prefix
 EquipRecommendedGear = app.api	-- Create a namespace for our "API"
+local api = app.api
 local L = app.locales
 
 ---------------------------
