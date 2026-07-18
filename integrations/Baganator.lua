bc0626b0fb6b9ec1f9550433edac6b4b138f5675
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-07-18 13:46:27 +0200

    Refresh Baganator on player equipment changed (hopefully fixes catalyst bug)

--- integrations/Baganator.lua
+++ integrations/Baganator.lua
@@ -1,13 +1,19 @@
 local appName, app = ...
 local api = app.api
 
--- Baganator integration
 EventUtil.ContinueOnAddOnLoaded("Baganator", function()
 	Baganator.API.RegisterUpgradePlugin("Equip Recommended Gear", "equip_recommended_gear", function(itemLink)
 		return api:IsItemUpgrade(itemLink)
 	end)
 
 	app.Event:Register("PLAYER_LEVEL_UP", function(level, healthDelta, powerDelta, numNewTalents, numNewPvpTalentSlots, strengthDelta, agilityDelta, staminaDelta, intellectDelta)
+		if Baganator.API.IsUpgradePluginActive("equip_recommended_gear") then
+			app.IsItemEquippable = {}
+			Baganator.API.RequestItemButtonsRefresh()
+		end
+	end)
+
+	app.Event:Register("PLAYER_EQUIPMENT_CHANGED", function(equipmentSlot, hasCurrent)
 		if Baganator.API.IsUpgradePluginActive("equip_recommended_gear") then
 			app.IsItemEquippable = {}
 			Baganator.API.RequestItemButtonsRefresh()
